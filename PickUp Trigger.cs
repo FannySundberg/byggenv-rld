60140996c150620db4419cc5d97d2b1583ae2a83
FannySundberg <105210631+FannySundberg@users.noreply.github.com>
2022-05-10 13:06:36 +0200

    Add files via upload

---
+++ PickUp Trigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public class PickUpTrigger : MonoBehaviour
+//{
+   // public GameManager gm;
+
+  //  [SerializeField] AudioSource collectionSound;
+
+   // private void OnTriggerEnter(Collider other)
+   // {
+    //    if (other.CompareTag("Player"))
+       // {
+            //GameManager: lägg till en poäng 
+        //    gm.AddScore(1);
+
+          //  collectionSound.Play();
+
+         //   Destroy(this.gameObject);
+      //  }
+
+ //   }
+//}
